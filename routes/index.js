e7b6030a2c40e1e8252c8550ff604eee7bebe766
iigmir <roc120j@gmail.com>
2020-09-26 00:15:31 +0800

    Add route module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,22 @@
 import routes from "./routes.js";
 import controllers from "../controllers/index.js";
 
+const route_module = (
+    app,
+    route = { COLLECTION: "",MEMBER: "" },
+    func = (req, res, next) => {}
+) =>
+{
+    app.get( route.COLLECTION, (req, res, next) => func(req, res, next) );
+    app.get( route.MEMBER, (req, res, next) => func(req, res, next) );
+    app.put( route.MEMBER, (req, res, next) => func(req, res, next) );
+    app.post( route.MEMBER, (req, res, next) => func(req, res, next) );
+    app.delete( route.MEMBER, (req, res, next) => func(req, res, next) );
+};
+
 const main = (app) =>
 {
-    app.get( routes.cases.COLLECTION, (req, res, next) => controllers.cases(req, res, next) );
-    app.get( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
-    app.post( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
-    app.put( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
-    app.delete( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
+    route_module( app, routes.cases, controllers.cases );
 };
 
 export default main;
