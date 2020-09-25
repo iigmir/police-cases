cedfd3b408984b16529162e8a009115d6818a113
iigmir <roc120j@gmail.com>
2020-09-25 23:15:24 +0800

    Spilt modules

--- controllers/cases.js
+++ controllers/cases.js
@@ -1,13 +1,20 @@
 import query from "../model/cases.js";
 
+const status_code = ( status = "Error" ) =>
+{
+    const dictionary = {
+        "Error": 500,
+        "Not Found": 404
+    };
+    return dictionary[ status ] || 200;
+}
+
 export default async (req, res, next) =>
 {
     const id = req.params.id || "";
-    const { method } = req;
-    const data = await query();
-    res.status( data.status === "Error" ? 500 : 200 );
+    const data = await query( id );
+    res.status( status_code( data.status ) );
     res.send({
-        method,
         data,
         id,
     });
--- model/cases.js
+++ model/cases.js
@@ -1,22 +1,33 @@
 import mariadb from "mariadb";
 import setting from "../connection-setting.js";
 
-export default async() =>
+const requested_sql = ( id = NaN ) =>
+{
+    return !isNaN( id ) ? "SELECT * FROM cases WHERE cases.id = " + id : "SELECT * FROM cases LIMIT 5";
+};
+
+const status_message = ( data ) =>
+{
+    if( data instanceof Error )
+    {
+        return "Error";
+    }
+    if( data.length < 1 )
+    {
+        return "Not Found";
+    }
+    return "Success";
+};
+
+export default async( id = "" ) =>
 {
     try {
         const conn = await mariadb.createConnection( setting );
-        const data = await conn.query( "SELECT * FROM cases LIMIT 5", (err, rows) =>
+        const data = await conn.query( requested_sql( parseInt( id, 10 ) ) , (err, rows) =>
         {
-            if( err )
-            {
-                return err;
-            }
-            else
-            {
-                return rows;
-            }
+            return err ? err : rows;
         });
-        const status = data instanceof Error ? "Error" : "Success";
+        const status = status_message( data );
         return {
             status,
             data,
@@ -24,7 +35,7 @@ export default async() =>
     } catch (err) {
         return {
             status: "Error",
-            message: err,
+            data: err,
         };
     }
 };
