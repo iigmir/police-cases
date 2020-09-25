76155be3a049324ef2c7c091a9f8adf42b27b7d0
iigmir <roc120j@gmail.com>
2020-09-26 00:09:31 +0800

    Delete COLLECTION POST

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ import controllers from "../controllers/index.js";
 const main = (app) =>
 {
     app.get( routes.cases.COLLECTION, (req, res, next) => controllers.cases(req, res, next) );
-    app.post( routes.cases.COLLECTION, (req, res, next) => controllers.cases(req, res, next) );
     app.get( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
     app.post( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
     app.put( routes.cases.MEMBER, (req, res, next) => controllers.cases(req, res, next) );
